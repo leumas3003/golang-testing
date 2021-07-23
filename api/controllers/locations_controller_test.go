7a9a41e195f5094ca1dd9bcfdb02eaef09f8505f
samuel.martel@derivco.se <leumas3003@gmail.com>
2021-07-23 15:23:58 +0100

    [Samu] add gomock

--- api/controllers/locations_controller_test.go
+++ api/controllers/locations_controller_test.go
@@ -5,10 +5,12 @@ import (
 	"golang-testing/api/domain/locations"
 	"golang-testing/api/services"
 	"golang-testing/api/utils/errors"
+	mock "golang-testing/internal/mocks"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/golang/mock/gomock"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 )
@@ -74,4 +76,57 @@ func TestGetCountryNoError(t *testing.T) {
 
 	assert.EqualValues(t, "AR", country.Id)
 	assert.EqualValues(t, "Argentina", country.Name)
+}
+
+func TestGetCountryNotFoundMock(t *testing.T) {
+	//Create a mock with gomock
+	controller := gomock.NewController(t)
+	m := mock.NewMocklocationsServiceInterface(controller)
+	defer controller.Finish()
+
+	//Create echo Context
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/locations/countries/:country_id")
+
+	//Configure the mock
+	m.EXPECT().GetCountry(gomock.Eq("AR")).Return(nil, &errors.ApiError{
+		Status:  http.StatusNotFound,
+		Message: "Country not found",
+	})
+
+	_, err := m.GetCountry("AR")
+
+	assert.EqualValues(t, http.StatusNotFound, err.Status)
+	assert.EqualValues(t, http.StatusNotFound, err.Status)
+	assert.EqualValues(t, "Country not found", err.Message)
+
+}
+
+func TestGetCountryNotErrordMock(t *testing.T) {
+	//Create a mock with gomock
+	controller := gomock.NewController(t)
+	m := mock.NewMocklocationsServiceInterface(controller)
+	defer controller.Finish()
+
+	//Create echo Context
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/locations/countries/:country_id")
+
+	//Configure the mock
+	m.EXPECT().GetCountry(gomock.Eq("AR")).Return(&locations.Country{
+		Id:   "AR",
+		Name: "Argentina",
+	}, nil)
+
+	country, _ := m.GetCountry("AR")
+
+	assert.EqualValues(t, "AR", country.Id)
+	assert.EqualValues(t, "Argentina", country.Name)
+
 }
